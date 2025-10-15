612f7de9872d6b8296bc22a05bc685debd7572e2
irawo/star <irawo10@gmail.com>
2025-10-15 10:12:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,8 +48,8 @@ function setup() {
 function draw() {
   background(217, 238, 244);
   textSize(14);
-  text('cloud conversations at the sabey data center', windowWidth - 225, 25);
-  text(hour() + ":" + minute(), windowWidth - 55, 50);
+  text('cloud conversations at the sabey data center', windowWidth - 275, 25);
+  text(hour() + ":" + minute(), windowWidth - 105, 50);
 
   // info text specs
   textAlign(LEFT, TOP);
